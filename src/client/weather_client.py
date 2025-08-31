a0019131db7a0ff17d594473e2e6b9f8e16087b5
Daria Odintsova <daria.odintsova@partner.bmw.de>
2025-08-31 18:18:35 +0200

    Add k8s configs

--- src/client/weather_client.py
+++ src/client/weather_client.py
@@ -1,15 +1,20 @@
 import requests
 import time
 import os
+from pathlib import Path
 
 SERVER_ADDR = os.getenv("SERVER_ADDR")
 WEATHER_API_URL = f"http://{SERVER_ADDR}:5000/weather"
 CITY = "Belgrade"  # Change to your desired city
 
 def fetch_weather():
+    file_path = Path("/data/weather.txt")
+    file_path.parent.mkdir(parents=True, exist_ok=True)
     try:
         response = requests.get(WEATHER_API_URL, params={"city": CITY})
         if response.status_code == 200:
+            with open ("/data/weather.txt", "a") as f:
+                f.write(f"{time.ctime()}: {response.json()}\n")
             print("Weather:", response.json())
         else:
             print("Error:", response.json())
